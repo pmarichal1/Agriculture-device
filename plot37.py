48caa4c9cf25f73f7b5fc55a8259fcca7bd9c4d4
paul <pmarichal108@gmail.com>
2019-04-10 13:40:09 -0400

    newfile

---
+++ plot37.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 
+import os
+import sys
+import platform
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import pickle
+import time
+
+print(sys.version_info)
+print(platform.python_version())
+print(platform.platform())
+
+lock_hit=0
+while(1):
+    # check if lock file exist since it means file is being updated and we should not access it
+    if os.path.isfile('lock.txt'):
+        time.sleep(1)
+        lock_hit+=1
+    else:
+        with open('envfile.data', 'rb') as filehandle:  
+            # read the data as binary data stream
+            temp_list = pickle.load(filehandle)
+            hum_list = pickle.load(filehandle)
+        print(f"Lhum = {len(hum_list)},  ltemp = {len(temp_list)}")
+       
+        temp_elements = np.array(temp_list)
+        temp_mean = np.mean(temp_elements, axis=0)
+        temp_sd = np.std(temp_elements, axis=0)
+        print(f"******TEMP mean = {temp_mean:2.2f}   dev={temp_sd:2.2f} x={temp_mean + 1 * temp_sd:2.2f}")
+        temp_final_list = [x for x in temp_list if (x < (temp_mean+.5) + (1 * temp_sd))]
+        print(f"******TEMP mean = {temp_mean:2.2f}   dev={temp_sd:2.2f} x={temp_mean - (1 * temp_sd):2.2f}")
+        temp_final_list = [x for x in temp_final_list if (x > (temp_mean-.5) - (1 * temp_sd))]
+        yarr1 = list(range(len(temp_final_list)))
+
+        hum_elements = np.array(hum_list)
+        hum_mean = np.mean(hum_elements, axis=0)
+        hum_sd = np.std(hum_elements, axis=0)
+        print(f"******HUM  mean = {hum_mean:2.2f}   dev={hum_sd:2.2f}  x={hum_mean + 2 * hum_sd:2.2f}")
+        hum_final_list = [x for x in hum_list if (x < hum_mean + 2 * hum_sd)]
+        print(f"******HUM  mean = {hum_mean:2.2f}   dev={hum_sd:2.2f}  x={hum_mean - 2 * hum_sd:2.2f}")
+        hum_final_list = [x for x in hum_final_list if (x > hum_mean - 2 * hum_sd)]
+        yarr = list(range(len(hum_final_list)))
+        print(f"Lhum = {len(hum_final_list)},  ltemp = {len(temp_final_list)}")
+        
+        
+        plt.xlabel("Time (s)")
+        plt.ylabel("Temp (F)")
+        plt.plot(yarr, hum_final_list, yarr1, temp_final_list)
+        plt.draw()
+        print(f"---Plot graph finish---    Lock hit = {lock_hit}")
+        plt.ion()
+        plt.show()
+        time.sleep(3)
+        plt.pause(0.0001)
+        plt.clf()
+
